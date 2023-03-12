388a86c48e16af2aa870f7daa8e5211b590a97d2
ShinyMyst <ShinyMyst@gmail.com>
2023-03-12 00:51:06 -0500

    Setup file added.

---
+++ setup.py
@@ -0,0 +1,34 @@
+######################
+# Set-Up
+######################
+"""
+Prepares the Pygame Application window and loads all required resources.
+"""
+import pygame as pg
+import os
+from loaders import load_graphics
+
+# Default Settings
+screen_size = 1000, 500
+caption = "Exhumed"
+
+# Initialize Pygame
+pg.init()
+pg.display.set_caption(caption)
+SCREEN = pg.display.set_mode(screen_size)
+
+
+# Resource Directories
+current_dir = os.path.dirname(os.path.realpath(__file__))
+gfx_path = os.path.join(current_dir, "resources", "GFX")
+
+background_dir = os.path.join(gfx_path, "backgrounds")
+button_dir = os.path.join(gfx_path, "buttons")
+unit_dir = os.path.join(gfx_path, "units")
+
+# Create Resource Dictionaries
+GFX = {
+    "backgrounds": load_graphics(background_dir),
+    "buttons": load_graphics(button_dir),
+    "units": load_graphics(unit_dir)
+}
