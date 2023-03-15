8d063d468c83704d4c575217f7c9108d50900533
ShinyMyst <ShinyMyst@gmail.com>
2023-03-15 14:36:29 -0400

    Non functional sprites rendered.

--- SpriteKit/object.py
+++ SpriteKit/object.py
@@ -7,24 +7,74 @@ Base class template for all sprite style objects used on screen.
 import pygame as pg
 
 
-class _Object(pg.sprite.Sprite):
-    """Template for all sprites on screen."""
+class _StaticObject(pg.sprite.Sprite):
+    """Template for sprite with no functionality."""
     def __init__(self, image: pg.Surface, size: tuple[int, int]):
+        super().__init__()
         self.image = pg.transform.scale(image, size)
         self.mask = pg.mask.from_surface(self.image.convert_alpha())
 
         self.rect = self.image.get_rect()
         self.size_x, self.size_y = size
-        self.rect.x, self.rect.y = None
+        #self.rect.x = None
+        #self.rect.y = None
 
     #####################################
     # Getters/Setters
     #####################################
     def set_pos(self, pos):
-        self.rect[0], self.rect[1] = pos
+        self.rect.x, self.rect.y = pos
 
     def get_coords(self):
         return self.rect
 
     def get_size(self):
         return self.size_x, self.size_y
+
+
+class Button(_StaticObject):
+    """Clickable object."""
+    def __init__(self, image: pg.Surface, size: tuple[int, int]):
+        super().__init__(image, size)
+        self.mask = pg.mask.from_surface(self.image.convert_alpha())
+
+        self.image_base = pg.transform.scale(image, size)
+        self.image_active = pg.transform.scale(image, size)
+        self.image_active.fill((225, 250, 255, 100), special_flags = pg.BLEND_MULT)
+
+
+        self.function = None
+
+        self._is_hovered = False
+
+    #####################################
+    # Getters/Setters
+    #####################################
+
+    def set_function(self, function):
+        self.function = function
+
+
+    def check_collision(self, mouse_pos):
+        """Updates variable determining if sprite is being hovered over."""
+        if self.rect.collidepoint(mouse_pos):
+            pos_in_mask = mouse_pos[0] - self.rect.x, mouse_pos[1] - self.rect.y
+            self._is_hovered = True if self.mask.get_at(pos_in_mask) else False
+        else:
+            self._is_hovered = False
+
+    def update_appearence(self):
+        """Change how sprite looks based on its current status."""
+        if self._is_hovered:
+            self.displayed_image = self.image_active
+        else:
+            self.displayed_image = self.image_base
+
+
+    def update(self, mouse_pos:pg.mouse):
+        """Updates sprites appearance on screen."""
+        self.check_collision(mouse_pos)
+        self.update_appearence()
+
+# Needs some refactoring
+# self.image is a name used by pg and can't be changed
--- states.py
+++ states.py
@@ -1,4 +1,7 @@
-from data import GFX
+from data import GFX, SCREEN
+from SpriteKit.object import Button
+import pygame as pg
+
 
 
 
@@ -16,11 +19,24 @@ class _State:
     def get_sprite_group(self):
         return self._sprite_group
 
+### Beyond this point is a seperate main menu state file in future ###
+
+start_button = Button(GFX['buttons']['start'], (200, 100))
+start_button.set_pos((50, 50))
+start_button.set_function(None)
+main_menu_group = [
+    start_button
+]
+
+
+
+main_menu_sprites = pg.sprite.RenderUpdates()
+main_menu_sprites.add(start_button)
 
 class MainMenu(_State):
     def __init__(self):
         self._bg = GFX['backgrounds']['back_cave']
-        self._sprite_group = None
+        self._sprite_group = main_menu_sprites
 
 
 
